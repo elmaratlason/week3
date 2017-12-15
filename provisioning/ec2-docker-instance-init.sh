d9454fdc48107dd85e8dcde8bb129d9bbbecd730
Elmar Atlason <elmar.atlason@gmail.com>
2017-12-15 10:48:47 +0000

    first friday build

--- provisioning/ec2-docker-instance-init.sh
+++ provisioning/ec2-docker-instance-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+sudo exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 sudo yum -y update
 sudo yum -y install docker
@@ -9,5 +9,8 @@ sudo pip install backports.ssl_match_hostname --upgrade
 
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+
+# install data dog install_agent
+DD_API_KEY=5227161a7f5d85725705c0348174953f bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)"
 
 touch ec2-init-done.markerfile
---
+++ week3.md
@@ -0,0 +1,17 @@
+# Week 3
+
+
+# DataDog
+
+agents for docker, ubuntu, jenkins installed
+https://app.datadoghq.com/
+
+for both ec2 instances, I added neccessary data dog steps
+- ec2-bootstrap-jenkins.sh
+- ./provisioning/ec2-docker-instance-init.sh
+
+## integrations
+- jenkins
+- github
+- docker
+- aws
